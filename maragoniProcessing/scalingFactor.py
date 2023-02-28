c7595af1e470d6fb585f096e9ba68cd9dfc24bce
unknown <sebastianschauerpsn@outlook.com>
2023-02-28 14:56:32 +0100

    Edited scalingFactor.py to calculate mean distance per pixel

--- maragoniProcessing/scalingFactor.py
+++ maragoniProcessing/scalingFactor.py
@@ -14,8 +14,28 @@ def scalingFactor(calib_img: np.ndarray, patternSize: tuple, checkerSize: int) -
     ret, corners = cv2.findChessboardCorners(img, patternSize, None)
 
     if ret:
-        # Euclidian norm between the first to corners
-        d = np.linalg.norm(corners[1]-corners[0])
+        width = patternSize[0]
+        height = patternSize[1]
+        distanceValues = np.zeros(width*(width-1)+height*(height-1)) 
+        # Vertical values
+        i = 0 # corner counter
+        n = 0 # value counter
+        while i < (width*height):
+            if (i+1)%height !=0:
+            # Euclidian norm
+                distanceValues[n]= np.linalg.norm(corners[i+1]-corners[i])
+                n+=1
+            i+=1
+        # Horizontal values
+        i = 0 # corner counter
+        while i < ((width*height)-height):
+            # Euclidian norm
+            distanceValues[n]= np.linalg.norm(corners[i+height]-corners[i])
+            n+=1
+            i+=1
+        d = np.mean(distanceValues)
+        # Euclidian norm between the first to corners Deprecated
+        #d = np.linalg.norm(corners[1]-corners[0]) Deprecated
         # scaling factor
         scal = checkerSize/d
 
